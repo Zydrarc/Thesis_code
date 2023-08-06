06b29ce8d1f34083872833dfbaf48bb3da379aa2
Zydrarc <64429872+Zydrarc@users.noreply.github.com>
2023-08-06 11:24:21 +0200

    Add files via upload

--- Trophic levels.R
+++ Trophic levels.R
@@ -14,7 +14,7 @@ options(scipen=0) # scientific notation on
 options(scipen=999) # scientific notation off
 
 experiments = 5 # number of experiments run
-replicates = 10
+replicates = 20
 tstep= 200000
 tsave= 5000 # every how many timesteps the experiment is saved 
 timesteps = seq(0,tstep, by = tsave)
@@ -94,23 +94,26 @@ grouped_trophic_data_sum <- grouped_trophic_data_sum %>%
 
 # ADD THESE LINES
 # Convert wide data to long
+# Convert wide data to long
 long_data_pct <- grouped_trophic_data_sum %>% 
   gather(key = "Trophic_Level", value = "Percentage", Tl_1_pct:Tl_4_pct)
 
 color_palette <- viridis(length(unique(long_data_pct$Trophic_Level)))
-long_data_pct$Trophic_Level <- recode(long_data_pct$Trophic_Level,
-                                      `Tl_1_pct` = "1st trophic level",
-                                      `Tl_2_pct` = "2nd trophic level",
-                                      `Tl_3_pct` = "3rd trophic level",
-                                      `Tl_4_pct` = "4th trophic level")
+long_data_pct$Trophic_Level <- case_when(
+  long_data_pct$Trophic_Level == "Tl_1_pct" ~ "1st trophic level",
+  long_data_pct$Trophic_Level == "Tl_2_pct" ~ "2nd trophic level",
+  long_data_pct$Trophic_Level == "Tl_3_pct" ~ "3rd trophic level",
+  long_data_pct$Trophic_Level == "Tl_4_pct" ~ "4th trophic level"
+)
+
 
 # Plot
 pct_plot <- ggplot(long_data_pct, aes(x = Timestep, y = Percentage, fill = Trophic_Level)) +
   geom_bar(stat = "identity") +
   facet_wrap(~Experiment, scales = "free_x") +
-  labs(x = "Timestep", y = "Percentage (%)", fill = "Trophic Level") +
+  labs(x = "Timestep", y = "Relative Percentage (%)", fill = "Trophic Level", title = "Graph of relative percentage of trophic levels with increasing complexity") +
   scale_fill_manual(values = color_palette) +  # Set the color palette
-  scale_y_reverse() +  # Reverse the order of y-axis
+  #scale_y_reverse() +  # Reverse the order of y-axis
   theme_minimal()
 print(pct_plot)
 
@@ -125,3 +128,27 @@ ggplot(long_data_no_pct, aes(x = Timestep, y = Percentage, fill = Trophic_Level)
   labs(x = "Timestep", y = "Value", fill = "Trophic Level") +
   theme_minimal()
 
+# Reshape data to long format for boxplot
+trophic_data_long <- reshape2::melt(grouped_trophic_data_sum, id.vars = c("Experiment", "Timestep"),
+                                    variable.name = "Trophic_Level", value.name = "Value")
+
+# Filter the data for the trophic levels of interest
+trophic_data_filtered <- trophic_data_long %>%
+  filter(Trophic_Level %in% c("Tl_1_pct", "Tl_2_pct", "Tl_3_pct", "Tl_4_pct"))
+
+# Update the Trophic_Level labels
+trophic_data_filtered$Trophic_Level <- case_when(
+  trophic_data_filtered$Trophic_Level == "Tl_1_pct" ~ "1st trophic level",
+  trophic_data_filtered$Trophic_Level == "Tl_2_pct" ~ "2nd trophic level",
+  trophic_data_filtered$Trophic_Level == "Tl_3_pct" ~ "3rd trophic level",
+  trophic_data_filtered$Trophic_Level == "Tl_4_pct" ~ "4th trophic level",
+  TRUE ~ trophic_data_filtered$Trophic_Level
+)
+
+# Create a boxplot with experiments on x-axis
+ggplot(trophic_data_filtered, aes(x = factor(Experiment), y = Value, fill = Trophic_Level)) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(legend.position = "none") +
+  labs(x = "Experiment", y = "Trophic Prevalence (%)") +
+  facet_wrap(~Trophic_Level, scales = "free")
